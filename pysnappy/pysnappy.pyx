04caa164f80d3a0c7f72b366013ed8f8b3d794df
Evan McClain <aeroevan@gmail.com>
2016-01-27 18:23:44 -0500

    Refactoring changes.

--- pysnappy/pysnappy.pyx
+++ pysnappy/pysnappy.pyx
@@ -1,33 +1,34 @@
 from libc.stdlib cimport malloc, free
-cimport pysnappy_c
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+cimport pysnappy.pysnappy_c
 
 def uncompress(bytes compressed):
     cdef size_t n = len(compressed)
     cdef size_t m
-    cdef pysnappy_c.snappy_status status
-    cdef char* uncompressed = NULL
-    status = pysnappy_c.snappy_uncompressed_length(
+    cdef pysnappy.pysnappy_c.snappy_status status
+    cdef char* uncompressed
+    status = pysnappy.pysnappy_c.snappy_uncompressed_length(
         compressed, n, &m)
     if status != 0:
         raise Exception("Could not determine uncompressed length")
-    uncompressed = <char*>malloc(m * sizeof(char*))
-    if uncompressed == NULL:
+    uncompressed = <char*>PyMem_Malloc(m * sizeof(char*))
+    if not uncompressed:
         raise MemoryError("Could not allocate uncompressed buffer")
-    status = pysnappy_c.snappy_uncompress(compressed, n, uncompressed, &m)
+    status = pysnappy.pysnappy_c.snappy_uncompress(compressed, n, uncompressed, &m)
     if status != 0:
-        free(uncompressed)
+        PyMem_Free(uncompressed)
         raise Exception("Could not uncompress")
-    return uncompressed
+    return uncompressed[:m]
 
 def compress(bytes uncompressed):
     cdef size_t n = len(uncompressed)
-    cdef size_t m = pysnappy_c.snappy_max_compressed_length(n)
-    cdef pysnappy_c.snappy_status status
-    cdef char* compressed = NULL
-    compressed = <char*>malloc(m * sizeof(char*))
-    if compressed == NULL:
+    cdef size_t m = pysnappy.pysnappy_c.snappy_max_compressed_length(n)
+    cdef pysnappy.pysnappy_c.snappy_status status
+    cdef char* compressed
+    compressed = <char*>PyMem_Malloc(m * sizeof(char*))
+    if not compressed:
         raise MemoryError("Could not allocate compressed buffer")
-    status = pysnappy_c.snappy_compress(uncompressed, n, compressed, &m)
+    status = pysnappy.pysnappy_c.snappy_compress(uncompressed, n, compressed, &m)
     if status != 0:
         free(compressed)
         raise Exception("Could not compress")
